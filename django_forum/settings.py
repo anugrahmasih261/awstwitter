3c5c1ce6b26ceb245ae689ebd03e69d6cd5e6095
ANUGRAH MASIH <anugrahmasih1012@gmail.com>
2023-06-09 18:54:09 +0530

    Update settings.py

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -177,6 +177,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # this is added
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
